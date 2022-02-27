38f9179d50c4b8c81527db4f033d4d078e9177e0
sharansantosh97 <sharan.santosh.ss@gmail.com>
2022-02-26 19:55:11 -0800

    Navbar Created

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import NavigationBar from './components/NavigationBar';
+
+function App() {
+  return (
+    <NavigationBar/>
+  );
+}
+
+export default App;
