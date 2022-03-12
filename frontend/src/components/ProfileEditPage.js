12e0e50bb5c64965b84f3992de781ffbf68b525b
sharansantosh97 <sharan.santosh.ss@gmail.com>
2022-03-11 18:56:38 -0800

    Profile Edit Page Added

--- frontend/src/components/ProfileEditPage.js
+++ frontend/src/components/ProfileEditPage.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react';
 import '../styles/ProfileEditPage.css';
+import countries from '../countries';
 function ProfileEditPage() {
+
   return (
     <>
         <div class="container">
@@ -16,26 +18,26 @@ function ProfileEditPage() {
                     <div class="e-profile">
                     <div class="row">
                         <div class="col-12 col-sm-auto mb-3">
-                        <div class="mx-auto" style={{width: "140px"}}>
-                            <div class="d-flex justify-content-center align-items-center rounded" style={{height: "140px", backgroundColor: "rgb(233, 236, 239)"}}>
-                            <span style={{color: "rgb(166, 168, 170)", font: "bold 8pt Arial"}}>140x140</span>
+                        <div class="mx-auto" style={{width: "190px"}}>
+                            <div class="d-flex justify-content-center align-items-center rounded" style={{height: "190px", backgroundColor: "rgb(233, 236, 239)"}}>
+                            <span style={{color: "rgb(166, 168, 170)", font: "bold 8pt Arial"}}>190x190</span>
                             </div>
                         </div>
                         </div>
                         <div class="col d-flex flex-column flex-sm-row justify-content-between mb-3">
                         <div class="text-center text-sm-left mb-2 mb-sm-0">
-                            <h4 class="pt-sm-2 pb-1 mb-0 text-nowrap">John Smith</h4>
+                            <h4 class="pt-sm-2 pb-1 mb-0 text-nowrap" style = {{fontSize: "17px"}}>John Smith</h4>
                             <div class="mt-2">
-                            <button class="btn btn-primary" type="button">
+                            <button className="profilePicture-btn" style = {{fontSize: "12px"}} type="button">
                                 <i class="fa fa-fw fa-camera"></i>
-                                <span>Change Photo</span>
+                                <span style = {{fontSize: "12px"}}>Change Photo</span>
                             </button>
                             </div>
                         </div>
                         </div>
                     </div>
                     <ul class="nav nav-tabs">
-                        <li class="nav-item"><a href="" class="active nav-link">Profile Details</a></li>
+                        <li class="nav-item"><a href="" class="active nav-link">Details</a></li>
                     </ul>
                     <div class="tab-content pt-3">
                         <div class="tab-pane active">
@@ -51,8 +53,21 @@ function ProfileEditPage() {
                                 </div>
                                 <div class="col">
                                     <div class="form-group">
-                                    <label>Username</label>
-                                    <input class="form-control" type="text" name="username" placeholder="johnny.s" value="johnny.s"></input>
+                                    <label>Date of Birth</label>
+                                    <input class="form-control" type="date" name="dateOfBirth"></input>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label>Gender</label>
+                                        {/*<input class="form-check-input" type="radio" id="huey" name="drone" value="huey"checked></input>
+                                        <label for="huey">Huey</label>
+                                        <input class="form-check-input" type="radio" id="dewey" name="drone" value="dewey"></input>
+                                        <label for="dewey">Dewey</label>
+                                        <input class="form-check-input" type="radio" id="louie" name="drone" value="louie"></input>
+                                        <label for="louie">Louie</label>*/}
                                     </div>
                                 </div>
                                 </div>
@@ -61,6 +76,12 @@ function ProfileEditPage() {
                                     <div class="form-group">
                                     <label>Email</label>
                                     <input class="form-control" type="text" placeholder="user@example.com"></input>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                    <label>Phone Number</label>
+                                    <input class="form-control" type="tel" placeholder="123-456-7890" /*pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"*/></input>
                                     </div>
                                 </div>
                                 </div>
@@ -72,32 +93,48 @@ function ProfileEditPage() {
                                     </div>
                                 </div>
                                 </div>
-                            </div>
-                            </div>
-                            <div class="row">
-                            <div class="col-12 col-sm-6 mb-3">
-                                <div class="mb-2"><b>Change Password</b></div>
                                 <div class="row">
                                 <div class="col">
                                     <div class="form-group">
-                                    <label>Current Password</label>
-                                    <input class="form-control" type="password" placeholder="••••••"></input>
+                                    <label>Address Line 1</label>
+                                    <input class="form-control" type="text"></input>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                    <label>Address Line 2</label>
+                                    <input class="form-control" type="text"></input>
                                     </div>
                                 </div>
                                 </div>
                                 <div class="row">
                                 <div class="col">
                                     <div class="form-group">
-                                    <label>New Password</label>
-                                    <input class="form-control" type="password" placeholder="••••••"></input>
+                                    <label>City</label>
+                                    <input class="form-control" type="text"></input>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                    <label>State</label>
+                                    <input class="form-control" type="text"></input>
                                     </div>
                                 </div>
                                 </div>
                                 <div class="row">
                                 <div class="col">
                                     <div class="form-group">
-                                    <label>Confirm <span class="d-none d-xl-inline">Password</span></label>
-                                    <input class="form-control" type="password" placeholder="••••••"></input>
+                                    <label>Country</label>
+                                    <select class="form-control">
+                                     <option value="Select the country" /*onChange={handleCountryChange}*/>Select the country</option>
+                                     {countries.map((country) => <option value={country.name}>{country.name}</option>)}
+                                    </select>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                    <label>Zipcode</label>
+                                    <input class="form-control" type="text"></input>
                                     </div>
                                 </div>
                                 </div>
